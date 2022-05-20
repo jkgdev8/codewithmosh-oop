6e7853ac702b925b0445a9eb1b125804792f514d
jkgdev8 <jkgdev8@gmail.com>
2022-05-19 23:46:58 -0700

    oop encapsulation

---
+++ index.js
@@ -0,0 +1,24 @@
+// OOP vs regular variables and functions
+let baseSalary = 30_000;
+let overtime = 10;
+let rate = 20;
+
+function getWage(baseSalary, overtime, rate) {
+    return baseSalary + (overtime * rate);
+}
+
+//vs
+// OOP Encapsulation
+let employee = {
+    //propeties
+    baseSalary: 30_000,
+    overtime: 10,
+    rate: 20,
+    // methods
+    getWage: function() {
+        return this.baseSalary + (this.overtime * this.rate);
+    }
+
+};
+
+employee.getWage();
