681f9844d71d47e36105a13f6a6da36624e22932
jkgdev8 <jkgdev8@gmail.com>
2022-05-21 23:36:02 -0700

    oop

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-// OOP vs regular variables and functions
-let baseSalary = 30_000;
-let overtime = 10;
-let rate = 20;
+// // OOP vs regular variables and functions
+// // OOP Procederal
+// let baseSalary = 30_000;
+// let overtime = 10;
+// let rate = 20;
 
-function getWage(baseSalary, overtime, rate) {
-    return baseSalary + (overtime * rate);
-}
+// // too many parameters
+// function getWage(baseSalary, overtime, rate) {
+//     return baseSalary + (overtime * rate);
+// }
 
-//vs
-// OOP Encapsulation
-let employee = {
-    //propeties
-    baseSalary: 30_000,
-    overtime: 10,
-    rate: 20,
-    // methods
-    getWage: function() {
-        return this.baseSalary + (this.overtime * this.rate);
-    }
+// //vs
+// // OOP Encapsulation
+// let employee = {
+//     //propeties
+//     baseSalary: 30_000,
+//     overtime: 10,
+//     rate: 20,
+//     // methods
+//     getWage: function() {
+//         return this.baseSalary + (this.overtime * this.rate);
+//     }
 
-};
+// };
 
-employee.getWage();
+// employee.getWage();
+
+console.log('hello world');
 
 
-// OOp Abstraction
-//
