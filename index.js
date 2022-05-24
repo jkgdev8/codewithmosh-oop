f4f10976d50757ee5a842570630787d4b33ea077
jkgdev8 <jkgdev8@gmail.com>
2022-05-23 23:36:04 -0700

    function

--- index.js
+++ index.js
@@ -30,15 +30,56 @@
 // ES6 has classes
 
 // Object Literals
-const circle = {
-    radius: 1,
-    location:{
-        x: 1, 
-        y: 1
-    },
-    draw: function(){
-        console.log('draw');
-    }
-};
+// const circle = {
+//     radius: 1,
+//     location:{
+//         x: 1, 
+//         y: 1
+//     },
+//     draw: function(){
+//         console.log('draw');
+//     }
+// };
 
-circle.draw();
+// circle.draw();
+
+
+
+
+// Factory functions
+// function createCircle(radius){
+//     return{
+//         radius,
+//         draw: function(){
+//             console.log('still works');
+//         }
+//     }
+// }
+
+// const circle = createCircle(1);
+
+// console.log(circle);
+// console.log(circle.draw(0));
+
+
+
+// Constructor functions
+// function Circle (radius){
+//     console.log('this', this);
+//     this.radius = radius,
+//     this.draw = function(){
+//         console.log('draw');
+//     }
+// }
+
+// // the new will reference the latter object cause of 'this' without it it will reference the window
+// const another = new Circle(1)
+
+// console.log(another);
+
+
+
+
+
+
+
