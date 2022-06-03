61acaa5f42cbd9ccffa35931a134c7a4dc28f7ee
jkgdev8 <jkgdev8@gmail.com>
2022-06-03 00:42:17 -0700

    exercise

--- index.js
+++ index.js
@@ -154,21 +154,30 @@
 
 
 // Excercise Stopwatch
+// My Method
 
 function Stopwatch(){
     let startTime, endTime, running, duration = 0;
+    
+    Object.defineProperty(this, 'duration',{
+        get: function(){
+            return duration;
+        }
 
-    this.start = function(){
-        if (running)
+    });
+}
+
+Stopwatch.prototype.start = function(){
+    if (running)
         throw new Error('Stopwatch has already started.');
 
         running = true;
 
         startTime = new Date();
-    };
+}
 
-    this.stop = function(){
-        if (!running)
+Stopwatch.prototype.stop = function(){
+    if (!running)
          throw new Error('Stopwatch has not started.');
 
          running = false;
@@ -178,25 +187,14 @@ function Stopwatch(){
          const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
          duration += seconds;
 
-    };
+}
 
-    this. reset = function(){
-        startTime = null;
+Stopwatch.prototype.reset = function(){
+    startTime = null;
         endTime = null;
         running = false;
         duration = 0;
-
-    };
-
-    Object.defineProperty(this, 'duration',{
-        get: function(){
-            return duration;
-        }
-
-    });
 }
-
-
 
 
 
