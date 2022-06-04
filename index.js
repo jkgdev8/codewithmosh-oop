c1915839577c20ecacaae633a1c1b9d193d79376
jkgdev8 <jkgdev8@gmail.com>
2022-06-04 00:21:17 -0700

    mosh method

--- index.js
+++ index.js
@@ -156,6 +156,51 @@
 // Excercise Stopwatch
 // My Method
 
+// function Stopwatch(){
+//     let startTime, endTime, running, duration = 0;
+    
+//     Object.defineProperty(this, 'duration',{
+//         get: function(){
+//             return duration;
+//         }
+
+//     });
+// }
+
+// Stopwatch.prototype.start = function(){
+//     if (this.running)
+//         throw new Error('Stopwatch has already started.');
+
+//         this.runningrunning = true;
+
+//         startTime = new Date();
+// }
+
+// Stopwatch.prototype.stop = function(){
+//     if (!running)
+//          throw new Error('Stopwatch has not started.');
+
+//          running = false;
+
+//          endTime = new Date();
+
+//          const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+//          duration += seconds;
+
+// }
+
+// Stopwatch.prototype.reset = function(){
+//     startTime = null;
+//         endTime = null;
+//         running = false;
+//         duration = 0;
+// }
+
+// // document
+
+
+// Mosh Method
+
 function Stopwatch(){
     let startTime, endTime, running, duration = 0;
     
@@ -163,15 +208,29 @@ function Stopwatch(){
         get: function(){
             return duration;
         }
-
+    });
+    Object.defineProperty(this, 'startTime',{
+        get: function(){
+            return startTime;
+        }
+    });
+    Object.defineProperty(this, 'endTime',{
+        get: function(){
+            return endTime;
+        }
+    });
+    Object.defineProperty(this, 'running',{
+        get: function(){
+            return running;
+        }
     });
 }
 
 Stopwatch.prototype.start = function(){
-    if (running)
+    if (this.running)
         throw new Error('Stopwatch has already started.');
 
-        running = true;
+        this.runningrunning = true;
 
         startTime = new Date();
 }
@@ -195,9 +254,6 @@ Stopwatch.prototype.reset = function(){
         running = false;
         duration = 0;
 }
-
-// document
-
 
 
 
