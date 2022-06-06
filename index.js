f1ef83ca9aeb8fb9e7a438a03056798d7f997102
jkgdev8 <jkgdev8@gmail.com>
2022-06-06 00:03:36 -0700

    reset the constructor

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-// // Prototype Inheritance
+// // // Prototype Inheritance
 
+// function Shape(){
+// }
+
+// Shape.prototype.duplicate = function(){
+//     console.log('duplicate');
+// }
+
+
+
+
+// function Circle(radius){
+//     this.radius = radius;
+// }
+
+// Circle.prototype = Object.create(Shape.prototype)
+
+
+// Circle.prototype.draw = function(){
+//     console.log('draw');
+// }
+
+// const s = new Shape();
+// const c = new Circle(1);
+// const x = new Circle(2);
+
+
+
+// Resetting the Constructor
 function Shape(){
 }
 
@@ -15,6 +43,9 @@ function Circle(radius){
 }
 
 Circle.prototype = Object.create(Shape.prototype)
+// When ever you reset the prototpy of an object, you should also reset the constructor
+Circle.prototype.constructor = Circle;
+
 
 Circle.prototype.draw = function(){
     console.log('draw');
@@ -23,4 +54,3 @@ Circle.prototype.draw = function(){
 const s = new Shape();
 const c = new Circle(1);
 const x = new Circle(2);
-
