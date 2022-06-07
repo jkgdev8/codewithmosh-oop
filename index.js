87b3beb2c8cd0013cf7a002c41e968a09948f76b
jkgdev8 <jkgdev8@gmail.com>
2022-06-06 23:09:35 -0700

    Intermediate Function Inheritance

--- index.js
+++ index.js
@@ -27,30 +27,104 @@
 
 
 
-// Resetting the Constructor
-function Shape(){
+// // Resetting the Constructor
+// function Shape(){
+// }
+
+// Shape.prototype.duplicate = function(){
+//     console.log('duplicate');
+// }
+
+
+
+
+// function Circle(radius){
+//     this.radius = radius;
+// }
+
+// Circle.prototype = Object.create(Shape.prototype)
+// // When ever you reset the prototype of an object, you should also reset the constructor
+// Circle.prototype.constructor = Circle;
+
+
+// Circle.prototype.draw = function(){
+//     console.log('draw');
+// }
+
+// const s = new Shape();
+// const c = new Circle(1);
+// const x = new Circle(2);
+
+
+
+// // Calling the Super Constructor
+// function Shape(color){
+//     this.color = color;
+// }
+
+// Shape.prototype.duplicate = function(){
+//     console.log('duplicate');
+// }
+
+
+// function Circle(radius, color){
+//     // Super Constructor
+//     Shape.call(this, color);
+//     this.radius = radius;
+// }
+
+// Circle.prototype = Object.create(Shape.prototype)
+// // When ever you reset the prototype of an object, you should also reset the constructor
+// Circle.prototype.constructor = Circle;
+
+
+// Circle.prototype.draw = function(){
+//     console.log('draw');
+// }
+
+// const s = new Shape();
+// const c = new Circle(1, 'red');
+
+
+
+
+
+
+
+// Intermediate Function Inheritance
+function Shape(color){
+    this.color = color;
 }
 
 Shape.prototype.duplicate = function(){
     console.log('duplicate');
 }
 
+function extend(Child, Parent){
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
+    
+}
 
-
-
-function Circle(radius){
+function Circle(radius, color){
+    Shape.call(this, color);
     this.radius = radius;
 }
 
-Circle.prototype = Object.create(Shape.prototype)
-// When ever you reset the prototype of an object, you should also reset the constructor
-Circle.prototype.constructor = Circle;
+extend(Circle, Shape);
 
 
 Circle.prototype.draw = function(){
     console.log('draw');
 }
 
+function Square(size){
+    this.size = size;
+
+}
+extend(Square, Shape);
+
+
 const s = new Shape();
-const c = new Circle(1);
-const x = new Circle(2);
+const c = new Circle(1, 'red');
+
