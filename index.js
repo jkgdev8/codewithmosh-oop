7ab5a15d58cf4c99acb81a56535adb5a493851ed
jkgdev8 <jkgdev8@gmail.com>
2022-06-07 13:52:18 -0700

    method overriding

--- index.js
+++ index.js
@@ -91,40 +91,82 @@
 
 
 
-// Intermediate Function Inheritance
-function Shape(color){
-    this.color = color;
-}
+// // Intermediate Function Inheritance
+// function Shape(color){
+//     this.color = color;
+// }
 
-Shape.prototype.duplicate = function(){
-    console.log('duplicate');
-}
+// Shape.prototype.duplicate = function(){
+//     console.log('duplicate');
+// }
+
+// function extend(Child, Parent){
+//     Child.prototype = Object.create(Parent.prototype);
+//     Child.prototype.constructor = Child;
+    
+// }
+
+// function Circle(radius, color){
+//     Shape.call(this, color);
+//     this.radius = radius;
+// }
+// extend(Circle, Shape);
+
+
+
+// Circle.prototype.draw = function(){
+//     console.log('draw');
+// }
+
+// function Square(size){
+//     this.size = size;
+
+// }
+// extend(Square, Shape);
+
+
+// const s = new Shape();
+// const c = new Circle(1, 'red');
+// const sq = new Square('blue');
+
+
+
+
+
+// Method Overriding
+
 
 function extend(Child, Parent){
     Child.prototype = Object.create(Parent.prototype);
     Child.prototype.constructor = Child;
     
 }
+function Shape(){
+    
+}
 
-function Circle(radius, color){
-    Shape.call(this, color);
-    this.radius = radius;
+Shape.prototype.duplicate = function(){
+    console.log('duplicate');
+}
+
+
+function Circle(){
+
 }
 
 extend(Circle, Shape);
 
-
-Circle.prototype.draw = function(){
-    console.log('draw');
+Circle.prototype.duplicate = function(){
+    console.log('duplicate circle');
 }
 
-function Square(size){
-    this.size = size;
 
-}
-extend(Square, Shape);
+const c = new Circle(1);
 
 
-const s = new Shape();
-const c = new Circle(1, 'red');
+
+
+
+
+
 
