81d8159fbd248602b8d58c9c93ad29d5cc5cb61b
jkgdev8 <jkgdev8@gmail.com>
2022-06-23 15:14:33 -0700

    stack exercise

--- index.js
+++ index.js
@@ -280,6 +280,46 @@
 
 // My Method
 
+const _items = new WeakMap();
+class Stack{
+    constructor() {
+        _items.set(this, []);
+        
+    }
+
+    push(obj){
+        const items = _items.get(this);
+        items.push(obj);
+
+    }
+
+    pop(){
+        const items = _items.get(this);
+        if(items === 0)
+         throw new Error('Stack is empty')
+        return items.pop(); 
+
+        
+    }
+
+    peek(){
+        const items = _items.get(this);
+        if(items === 0)
+         throw new Error('Stack is empty')
+        
+        return items[items.length - 1];
+        
+    }
+
+    get count(){
+        const items = _items.get(this);
+        return items.length
+
+    }
+}
+
+const stack = new Stack();
+
 
 
 
