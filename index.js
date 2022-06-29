eccdac4e942ef5e9555a65d46bc2b2d3e819e099
jkgdev8 <jkgdev8@gmail.com>
2022-06-28 23:09:51 -0700

    es6 tooling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Modules
 
 // common js modules(node)
+
 // const Circle = require('./circle');
 
 // const c = new Circle(11);
@@ -12,11 +13,21 @@
 
 
 
-// ES6 Modules
+// ES6 Modules(browser)
 
-import { Circle } from "./circle.js";
+// import { Circle } from "./circle.js";
 
-const c = new Circle(11);
+// const c = new Circle(11);
 
-c.draw();
+// c.draw();
+
+
+
+// ES6 Tooling(browser) usually used for browser apps
+// transpiler(translator + compiler) = Babel(Tool that converts  modern javascript code into code that all browser understands)
+// Bundler(webpack)
+
+
+
+
 
