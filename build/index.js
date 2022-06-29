c5856d25e1b21bb7ed67193bc0608ce4de35bcea
jkgdev8 <jkgdev8@gmail.com>
2022-06-28 23:24:51 -0700

    babel

--- build/index.js
+++ build/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Modules
 
 // common js modules(node)
@@ -9,10 +11,6 @@
 // c.draw(); 
 
 
-
-
-
-
 // ES6 Modules(browser)
 
 // import { Circle } from "./circle.js";
@@ -22,22 +20,13 @@
 // c.draw();
 
 
-
 // ES6 Tooling(browser) usually used for browser apps
 // transpiler(translator + compiler) = Babel(Tool that converts  modern javascript code into code that all browser understands)
 // Bundler(webpack) = combines all javascript files into a single file(bundle) it minifies code
-
-
-
 
 
 // Babel
 // npm init -y (creates a file in the folder package.json, its an identifier for an application)
 // FLAG --save-dev it will install as development dependencies they will not be part of the app and be part of deployment
 
-const x = 1;
-
-
-
-
-
+var x = 1;
